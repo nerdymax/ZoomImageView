63674f90c3e1276b53e1a2c326f2bfa54d0ec914
Maximilian Bosch <max.bosch@outlook.com>
2016-12-01 03:06:24 +0100

    Initial commit

---
+++ app/src/main/java/dk/makeable/zoomimageview/ZoomImageView.java
@@ -0,0 +1,27 @@
+package dk.makeable.zoomimageview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Leo on 12/1/2016.
+ */
+
+public class ZoomImageView extends ImageView {
+    public ZoomImageView(Context context) {
+        super(context);
+    }
+
+    public ZoomImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ZoomImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ZoomImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
